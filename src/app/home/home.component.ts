4fe971a3a15136b228fc624fa3cb4d0f6504dbd5
Sudhansu Bhushan Mishra <13695177+the-lazy-learner@users.noreply.github.com>
2023-11-14 17:39:21 +0530

    Add search bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,9 +6,13 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results"></section>
   `,
   styleUrls: ['./home.component.css']
 })
