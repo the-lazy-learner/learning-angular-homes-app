e6cbde9fb1d40f345ddcc0f1ea73815a71ef5525
Sudhansu Bhushan Mishra <13695177+the-lazy-learner@users.noreply.github.com>
2023-11-14 18:03:12 +0530

    Build interface structure for housing location

--- src/app/housing-location.ts
+++ src/app/housing-location.ts
@@ -1,2 +1,10 @@
 export interface HousingLocation {
+    id: number,
+    name: string,
+    city: string,
+    state: string,
+    photo: string,
+    availableUnits: boolean,
+    wifi: boolean,
+    laundry: boolean,
 }
